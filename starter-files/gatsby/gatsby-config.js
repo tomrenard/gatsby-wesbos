920dab0296827448d922c7ab7f1c01582c754d97
Tom Renard <tom.renard@zoho.com>
2021-04-26 13:01:03 +0200

    new gatsby config

--- starter-files/gatsby/gatsby-config.js
+++ starter-files/gatsby/gatsby-config.js
@@ -6,7 +6,7 @@ export default {
   siteMetadata: {
     title: `Slicks Slices`,
     siteUrl: 'https://gatsby.pizza',
-    description: 'The best pizza place in Hamilton!',
+    description: 'The best pizza place in Berlin!',
     twitter: '@slicksSlices',
   },
   plugins: [
