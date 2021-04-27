99b2139381b29a95bbdf2fc28233c1d49d1c5e42
Tom Renard <tom.renard@zoho.com>
2021-04-27 15:38:20 +0200

    gatsby config update

--- starter-files/gatsby/gatsby-config.js
+++ starter-files/gatsby/gatsby-config.js
@@ -4,7 +4,7 @@ dotenv.config({ path: '.env' });
 
 export default {
   siteMetadata: {
-    title: `Slicks Slices`,
+    title: `Slicks Slices Tr`,
     siteUrl: 'https://gatsby.pizza',
     description: 'The best pizza place in Berlin!',
     twitter: '@slicksSlices',
